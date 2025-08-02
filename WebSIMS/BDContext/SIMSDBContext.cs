4b730a25e39f88fb37858baa7bdf36c09320a7f4
DuongDucX24 <157886979+DuongDucX24@users.noreply.github.com>
2025-08-02 15:11:47 +0700

    code of teacher

---
+++ WebSIMS/BDContext/SIMSDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebSIMS.BDContext.Entities;
+
+namespace WebSIMS.BDContext
+{
+    public class SIMSDBContext : DbContext
+    {
+        public SIMSDBContext(DbContextOptions<SIMSDBContext> options) : base(options) { }
+
+        public DbSet<Courses> CoursesDb { get; set; }
+        public DbSet<Users> UsersDb { get; set; }
+    }
+}
