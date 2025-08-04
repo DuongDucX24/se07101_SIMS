b77f7a33db10b7d77a29696f9a5f119b36e03ae0
DuongDucX24 <157886979+DuongDucX24@users.noreply.github.com>
2025-08-04 07:52:38 +0700

    .gitignore working, probably

---
+++ WebSIMS/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSIMS.Controllers
+{
+    public class CourseController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
